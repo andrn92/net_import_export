e5d617dfe1e03fda0bad2647b2514d91fca35097
andrn <01.nmeml@ya.ru>
2025-07-18 09:58:57 +0300

    change app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,3 @@
-import Game, {GameSavingData, readGameSaving as saveGame, writeGameSaving as loadGame} from './game.js'
+import Game, {GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame} from './game.js'
 const game = new Game()
 game.start()
